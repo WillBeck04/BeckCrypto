81e1537a57bcee2020186351a9f1453ec198ecce
Pawel Sobaniec <infipablosoban@gmail.com>
2023-04-23 13:11:59 +0200

    polish theme switch

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -1,58 +1,58 @@
-"use client";
+'use client'
 
-import { useState, useEffect } from "react";
-import { useTheme } from "next-themes";
-import { Popover } from "@headlessui/react";
-import { Monitor, Moon, Sun } from "lucide-react";
+import { useState, useEffect } from 'react'
+import { useTheme } from 'next-themes'
+import { Popover } from '@headlessui/react'
+import { Monitor, Moon, Sun } from 'lucide-react'
 
 export const ThemeSwitch = () => {
-  const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
 
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
-    setMounted(true);
-  }, []);
+    setMounted(true)
+  }, [])
 
   if (!mounted) {
-    return null;
+    return null
   }
 
   return (
     <Popover className="relative">
       <Popover.Button className="p-1 text-slate-600 dark:text-slate-400">
-        {theme === "light" ? (
-          <Moon className="w-6 h-6" />
+        {theme === 'light' ? (
+          <Moon className="h-6 w-6" />
         ) : (
-          <Sun className="w-6 h-6" />
+          <Sun className="h-6 w-6" />
         )}
       </Popover.Button>
 
       <Popover.Panel className="absolute z-10">
-        <div className="flex  rounded-md flex-col gap-3 text-xs dark:bg-slate-800 shadow-md bg-slate-200">
+        <div className="flex  flex-col gap-1 rounded-md bg-slate-200 py-3 text-xs shadow-md dark:bg-slate-800">
           <button
-            onClick={() => setTheme("system")}
-            className="flex gap-3 items-center font-semibold px-4 py-2 hover:dark:bg-slate-700 hover:bg-slate-300 w-full"
+            onClick={() => setTheme('system')}
+            className="flex w-full items-center gap-3 rounded-md px-8 py-1 font-semibold outline-none hover:bg-slate-300 hover:dark:bg-slate-700"
           >
-            <Monitor className="text-slate-600 dark:text-slate-400 w-5 h-5" />
+            <Monitor className="h-5 w-5 text-slate-600 dark:text-slate-400" />
             System
           </button>
           <button
-            onClick={() => setTheme("light")}
-            className="flex gap-3 items-center font-semibold px-4 py-2 hover:dark:bg-slate-700 hover:bg-slate-300 w-full"
+            onClick={() => setTheme('light')}
+            className="flex h-full w-full items-center gap-3 px-8 py-1 font-semibold hover:bg-slate-300 hover:dark:bg-slate-700"
           >
-            <Sun className="text-slate-600 dark:text-slate-400 w-5 h-5" />
+            <Sun className="h-5 w-5 text-slate-600 dark:text-slate-400" />
             Light
           </button>
           <button
-            onClick={() => setTheme("dark")}
-            className="flex gap-3 items-center font-semibold px-4 py-2 hover:dark:bg-slate-700 hover:bg-slate-300 w-full"
+            onClick={() => setTheme('dark')}
+            className="flex h-full w-full items-center gap-3 px-8 py-1 font-semibold hover:bg-slate-300 hover:dark:bg-slate-700"
           >
-            <Moon className="text-slate-600 dark:text-slate-400 w-5 h-5" />
+            <Moon className="h-5 w-5 text-slate-600 dark:text-slate-400" />
             Dark
           </button>
         </div>
       </Popover.Panel>
     </Popover>
-  );
-};
+  )
+}
