a6e64b53cf5fabe3c49756970fffcc14fc304814
Pawel Sobaniec <infipablosoban@gmail.com>
2023-04-26 20:21:04 +0200

    change color

--- src/app/portfolio/portfolio-list.tsx
+++ src/app/portfolio/portfolio-list.tsx
@@ -6,10 +6,10 @@ import { PortfolioContext } from '../providers'
 export default function PortfolioList() {
   const { portfolio } = useContext(PortfolioContext)
   return (
-    <div className='mb-6'>
+    <div className="mb-6">
       <h3 className="text-xl font-medium">Transactions</h3>
-      {portfolio.map((coin) => (
-        <div key={coin.name} className='flex space-x-3'>
+      {portfolio.map((coin, idx) => (
+        <div key={idx} className="flex space-x-3">
           <p>Price: {coin.price}</p>
           <p>Coin: {coin.name}</p>
           <p>Total Cost: {coin.cost}</p>
