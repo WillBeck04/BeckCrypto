6152772d2dd0a7b29328a8d5d5d15ae440202a7d
Pawel Sobaniec <infipablosoban@gmail.com>
2023-04-26 17:35:53 +0200

    add rows selector component

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -74,64 +74,70 @@ export function Pagination({ table }: { table: Table<CryptoData[number]> }) {
               />
             </span>
           </div>
-        </div>
-        <div>
-          <label className="text-sm">Show rows</label>
-          <Listbox
-            value={table.getState().pagination.pageSize}
-            onChange={(value) => {
-              table.setPageSize(Number(value))
-            }}
-          >
-            <div className="relative mt-1">
-              <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 dark:bg-slate-800 sm:text-sm">
-                <span className="block truncate">
-                  {table.getState().pagination.pageSize}
-                </span>
-                <span className="pointer-events-none absolute inset-y-0 right-2 flex items-center pr-2">
-                  <ChevronDown className="h-5 w-5" />
-                </span>
-              </Listbox.Button>
-              <Listbox.Options className="absolute right-10 z-10 mt-1 max-h-60 w-28 overflow-auto rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-slate-800 sm:text-sm">
-                {[10, 20, 30, 40, 50].map((pageSize) => (
-                  <Listbox.Option
-                    key={pageSize}
-                    value={pageSize}
-                    className={({ active }) =>
-                      `${
-                        active
-                          ? 'bg-indigo-500 text-white'
-                          : 'text-slate-900 dark:text-slate-100'
-                      } relative cursor-default select-none py-2 pl-10 pr-4`
-                    }
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <span
-                          className={`${
-                            selected ? 'font-semibold' : 'font-normal'
-                          } block truncate`}
-                        >
-                          {pageSize}
-                        </span>
-                        {selected ? (
-                          <span
-                            className={`${
-                              active ? 'text-white' : 'text-indigo-500'
-                            } absolute inset-y-0 left-0 flex items-center pl-3`}
-                          >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </div>
-          </Listbox>
+          <RowsSelector table={table} />
         </div>
       </div>
+    </div>
+  )
+}
+
+function RowsSelector({ table }: { table: Table<CryptoData[number]> }) {
+  return (
+    <div>
+      <label className="text-sm">Show rows</label>
+      <Listbox
+        value={table.getState().pagination.pageSize}
+        onChange={(value) => {
+          table.setPageSize(Number(value))
+        }}
+      >
+        <div className="relative mt-1">
+          <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 dark:bg-slate-800 sm:text-sm">
+            <span className="block truncate">
+              {table.getState().pagination.pageSize}
+            </span>
+            <span className="pointer-events-none absolute inset-y-0 right-2 flex items-center pr-2">
+              <ChevronDown className="h-5 w-5" />
+            </span>
+          </Listbox.Button>
+          <Listbox.Options className="absolute right-10 z-10 mt-1 max-h-60 w-28 overflow-auto rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-slate-800 sm:text-sm">
+            {[10, 20, 30, 40, 50].map((pageSize) => (
+              <Listbox.Option
+                key={pageSize}
+                value={pageSize}
+                className={({ active }) =>
+                  `${
+                    active
+                      ? 'bg-indigo-500 text-white'
+                      : 'text-slate-900 dark:text-slate-100'
+                  } relative cursor-default select-none py-2 pl-10 pr-4`
+                }
+              >
+                {({ selected, active }) => (
+                  <>
+                    <span
+                      className={`${
+                        selected ? 'font-semibold' : 'font-normal'
+                      } block truncate`}
+                    >
+                      {pageSize}
+                    </span>
+                    {selected ? (
+                      <span
+                        className={`${
+                          active ? 'text-white' : 'text-indigo-500'
+                        } absolute inset-y-0 left-0 flex items-center pl-3`}
+                      >
+                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    ) : null}
+                  </>
+                )}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </div>
+      </Listbox>
     </div>
   )
 }
